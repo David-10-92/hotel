4a9eec7dc8b5789570ec79d5747d3701177c3f00
DavidCazalla <ncedcb@outlook.es>
2024-06-20 14:14:40 +0200

    creacion de servicio para login usuario, implemetancion en modelo user el userdetails

--- src/main/java/com/hotel/model/User.java
+++ src/main/java/com/hotel/model/User.java
@@ -4,19 +4,27 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @NoArgsConstructor @Setter @Getter
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    @Column(unique = true, nullable = false)
     private String email;
     private String password;
     private String rol;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(rol));
+    }
 }
